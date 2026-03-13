53fa7c5b4bbf4fb28eabe3128cbd21fadb45f753
Daryna Lytvynenko <darynalytvynenko@gmail.com>
2026-03-13 12:23:34 +0000

    added tests

--- src/pages/CreateArticlePage.js
+++ src/pages/CreateArticlePage.js
@@ -7,6 +7,11 @@ export class CreateArticlePage {
       name: 'Publish Article',
     });
     this.errorMessage = page.getByRole('list').nth(1);
+    this.articleTitle = page.getByPlaceholder('Article Title');
+    this.articleDescription = page.getByPlaceholder(
+      'What\'s this article about?');
+    this.articleText = page.getByPlaceholder('Write your article (in');
+    this.articleTags = page.getByPlaceholder('Enter tags');
   }
 
   async clickPublishArticleButton() {
@@ -18,6 +23,31 @@ export class CreateArticlePage {
   async assertErrorMessageContainsText(messageText) {
     await test.step(`Assert the '${messageText}' error is shown`, async () => {
       await expect(this.errorMessage).toContainText(messageText);
+    });
+  }
+  async fillArticleTitle(text){
+    await test.step(`fillArticleTitle`, async () => {
+    await this.articleTitle.fill(text);
+   });
+  }
+  async fillArticleDescription(text){
+    await test.step(`fillArticleDescription`, async () => {
+    await this.articleDescription.fill(text);
+    });
+  }
+  async fillArticleText(text){
+    await test.step(`fillArticleText`, async () => {
+    await this.articleText.fill(text);
+    });
+  }
+  async fillArticleTags(text){
+    await test.step(`fillArticleTags`, async () => {
+    await this.articleTags.fill(text);
+    });
+  }
+  async pressEnter(){
+    await test.step(`pressEnter`, async () => {
+    await this.page.keyboard.press('Enter');
     });
   }
 }
---
+++ tests/createArticle/createArticleWithRequiredAndOptionalFields.spec.js
@@ -0,0 +1,39 @@
+import { test } from '@playwright/test';
+import { SignUpPage } from '../../src/pages/SignUpPage';
+import { HomePage } from '../../src/pages/HomePage';
+import { CreateArticlePage } from '../../src/pages/CreateArticlePage';
+import { faker } from '@faker-js/faker';
+
+let homePage;
+let createArticlePage;
+
+test.beforeEach(async ({ page }) => {
+  const signUpPage = new SignUpPage(page);
+  homePage = new HomePage(page);
+  createArticlePage = new CreateArticlePage(page);
+
+  const user = {
+    username: `${faker.person.firstName()}_${faker.person.lastName()}`,
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+  };
+
+  await signUpPage.open();
+  await signUpPage.fillUsernameField(user.username);
+  await signUpPage.fillEmailField(user.email);
+  await signUpPage.fillPasswordField(user.password);
+  await signUpPage.clickSignUpButton();
+  await homePage.assertYourFeedTabIsVisible();
+});
+
+test('Creat an article without required fields', async () => {
+  const text = 'test';
+  await homePage.clickNewArticleLink();
+  await createArticlePage.fillArticleTitle(text);
+  await createArticlePage.fillArticleDescription(text);
+  await createArticlePage.fillArticleText(text);
+  await createArticlePage.fillArticleTags(text);
+  await createArticlePage.pressEnter();
+  await createArticlePage.clickPublishArticleButton();
+});
+
